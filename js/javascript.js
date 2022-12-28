13c5203b89dcea0e80deb2ef8099018cef4b3472
Danilo <ddggmy2@gmail.com>
2022-12-28 08:21:06 -0500

    Boton desencriptar, Boton copiar

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,61 @@
 function encriptar() {
- document.getElementById("section2-elementos").style.display = "none";
+  document.getElementById("section2-elementos").style.display = "none";
 
   var inputsc2 = document.getElementById("section2-textarea");
   inputsc2.style.cssText =
     "display:block; font-family: 'Inter';font-style: normal;font-weight: 400;font-size: 24px;line-height: 150%;color: #495057;border:none; padding:0;width:80%;height:80%;position:relative;";
 
+  var pasartexto = document
+    .getElementById("section1-textarea")
+    .value.toLowerCase();
 
-
-  var pasartexto = document.getElementById("section1-textarea").value.toLowerCase();
-
-
-var cifrar = pasartexto.replace(/e/igm,"enter");
-var cifrar = cifrar.replace(/o/igm,"ober");
-var cifrar = cifrar.replace(/i/igm,"imes");
-var cifrar = cifrar.replace(/a/igm,"ai");
-var cifrar = cifrar.replace(/u/igm,"ufat");
+  var cifrar = pasartexto.replace(/e/gim, "enter");
+  var cifrar = cifrar.replace(/o/gim, "ober");
+  var cifrar = cifrar.replace(/i/gim, "imes");
+  var cifrar = cifrar.replace(/a/gim, "ai");
+  var cifrar = cifrar.replace(/u/gim, "ufat");
 
   document.getElementById("section2-textarea").value = cifrar;
 
   var btncopiar = document.getElementById("boton-copiar");
 
-  btncopiar.style.cssText =
-    "display:block;background: #D8DFE8;border: 1px solid #0A3871; border-radius: 24px;color: #0A3871;box-sizing: border-box; width: 22rem;height: 4rem;font-family: 'Inter';font-style: normal;font-weight: 400;font-size: 16px;line-height: 19px;position:absolute; bottom:7.9rem;";
+ btncopiar.style.cssText = "display:block;";
+
+
+
 }
 
+function desencriptar() {
+  /* document.getElementById("section2-elementos").style.display = "none"; */
+
+  var inputsc2 = document.getElementById("section2-textarea");
+  inputsc2.style.cssText =
+    "display:block; font-family: 'Inter';font-style: normal;font-weight: 400;font-size: 24px;line-height: 150%;color: #495057;border:none; padding:0;width:80%;height:80%;position:relative;";
+
+  var pasartexto = document
+    .getElementById("section1-textarea")
+    .value.toLowerCase();
+
+  var cifrar = pasartexto.replace(/enter/gim, "e");
+  var cifrar = cifrar.replace(/ober/gim, "o");
+  var cifrar = cifrar.replace(/imes/gim, "i");
+  var cifrar = cifrar.replace(/ai/gim, "a");
+  var cifrar = cifrar.replace(/ufat/gim, "u");
+
+  document.getElementById("section2-textarea").value = cifrar;
+
+  var btncopiar = document.getElementById("boton-copiar");
+  btncopiar.style.cssText =
+    "display:block;";
+
+
+}
+
+function copiar(){
+
+var inputsc2 = document.getElementById("section2-textarea");
+inputsc2.select();
+document.execCommand("copy");
+alert("se copió");
+
+}
